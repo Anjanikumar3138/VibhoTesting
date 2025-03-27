245520dfc0fbf905d262b0a48ea20ae4383caea4
Anjanikumar3138 <anjanikumar@vibhotech.com>
2025-03-27 16:31:56 +0530

    My First Commit 1

---
+++ src/test/java/demo1/Testcase1.java
@@ -0,0 +1,52 @@
+package demo1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+
+
+
+public class Testcase1 {
+	// First Test Case
+	
+	@Test
+   public static void users()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\ChromeDriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://in.dev.vibhohcm.com/");
+		
+		driver.findElement(By.id("username")).sendKeys("DEV");
+		driver.findElement(By.id("password")).sendKeys("12345");
+		driver.findElement(By.className("login-signin")).click();
+		driver.close();
+		
+	}
+	
+	//second test
+	@AfterTest
+	public void users2()
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\ChromeDriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://in.dev.vibhohcm.com/");
+		
+		driver.findElement(By.id("username")).sendKeys("dev0123");
+		driver.findElement(By.id("password")).sendKeys("1234567890");
+		driver.findElement(By.className("login-signin")).click();
+		driver.close();
+		
+	}
+}
